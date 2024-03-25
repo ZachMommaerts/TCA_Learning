62b665c85d2c3b26284fe614e77d6e0ce0e02302
ZachMommaerts <zmommaerts@gmail.com>
2024-03-24 23:56:25 -0500

    Initial Commit

---
+++ TCA_Learning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA_Learning
+//
+//  Created by Zach Mommaerts on 3/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCA_Learning/TCA_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_LearningApp.swift
+//  TCA_Learning
+//
+//  Created by Zach Mommaerts on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
